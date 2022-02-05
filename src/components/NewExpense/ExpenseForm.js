8ca2edc3ebbefbc91cc2e4d06a4fd3acf512ef00
Anurag <anurag.singh202@gmail.com>
2022-02-06 00:20:54 +0530

    error fixing

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -61,7 +61,7 @@ const ExpenseForm = (props) => {
           <input
             type="date"
             min="2020-01-01"
-            max="2022-12-31"
+            max="2024-12-31"
             value={enterdDate}
             onChange={dateChangedHandler}
           />
